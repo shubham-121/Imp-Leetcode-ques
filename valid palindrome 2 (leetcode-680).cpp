5379173b1d0bcd96c35db3bc412e9a337921be5f
Shubham Bhatt <81768689+shubham-121@users.noreply.github.com>
2023-08-19 10:06:34 +0530

    Create valid palindrome 2 (leetcode-680).cpp

---
+++ valid palindrome 2 (leetcode-680).cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool checkPalindrome(string s ,int i, int j)
+    {
+       
+        while(i<=j)
+        {
+            if(s[i] != s[j])
+            return false;
+            else{
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        int i=0; int j=s.size()-1;
+        while(i<=j)
+        {
+            if(s[i]==s[j])
+            {
+                i++;
+                j--;
+            }
+            else{
+               return  checkPalindrome(s, i+1, j) || checkPalindrome(s, i, j-1);
+            }
+        }
+        return true;
+    }
+};
