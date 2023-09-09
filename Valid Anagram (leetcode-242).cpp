96989f8627a8fe1f8820a954597c9d6e23194470
Shubham Bhatt <81768689+shubham-121@users.noreply.github.com>
2023-09-09 12:14:16 +0530

    Create Valid Anagram (leetcode-242).cpp

---
+++ Valid Anagram (leetcode-242).cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        unordered_map<char,int>umap;
+
+        for(auto it:s)
+        umap[it]++;
+
+        for(auto it:t)
+        umap[it]--;
+        
+        for(auto it:umap)
+        {
+          if(it.second!=0)
+          return false;
+        }
+        return true;
+        // sort(s.begin(), s.end());
+        // sort(t.begin(),t.end());
+       
+        // return (s==t);
+        
+    }
+};
